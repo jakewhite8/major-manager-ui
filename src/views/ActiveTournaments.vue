90a1c63a559dc3cd20cf13e7d5a95a347d49064e
Jake White <jwhite52@gmail.com>
2020-05-20 18:58:04 -0400

    Display tournament data in a table

--- src/views/ActiveTournaments.vue
+++ src/views/ActiveTournaments.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="container">
     <header class="jumbotron">
-      <h3>{{content}}</h3>
+      <h3>Active Tournaments</h3>
     </header>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm table-bordered table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th>Tournament</th>
+            <th>Start Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(tournament) in tournaments" :key="tournament.id">
+            <td>{{ tournament.name }}</td>
+            <td>{{ new Date(tournament.start_date).toString() }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -13,16 +29,16 @@ export default {
   name: 'ActiveTournaments',
   data() {
     return {
-      content: '',
+      tournaments: '',
     };
   },
   mounted() {
     TournamentService.getActiveTournaments().then(
       (response) => {
-        this.content = response.data;
+        this.tournaments = response.data;
       },
       (error) => {
-        this.content = (error.response && error.response.data)
+        this.tournaments = (error.response && error.response.data)
           || error.message
           || error.toString();
       },
