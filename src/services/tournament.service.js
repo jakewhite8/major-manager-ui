3d98fe8023c8f22239e91f137128e51e0ff7c421
Jake White <jwhite52@gmail.com>
2020-05-25 13:21:24 -0400

    Selected players are sent to server when form is submitted
    Players are organized by tier and when the form is submited, selected players are sent to an API along with userId and tournamentId

--- src/services/tournament.service.js
+++ src/services/tournament.service.js
@@ -1,17 +1,24 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8081/api/v1/';
+const API_URL = 'http://localhost:8081/api/v1';
 
 
 /* eslint class-methods-use-this: ["error", { "exceptMethods":
-['getActiveTournaments'] }] */
+['getActiveTournaments', 'getTournamentAndPlayerData', 'setUsersTournamentPlayers'] }] */
 class TournamentService {
   getActiveTournaments() {
-    return axios.get(`${API_URL}active_tournaments`, { headers: authHeader() });
+    return axios.get(`${API_URL}/active_tournaments`, { headers: authHeader() });
   }
+
   getTournamentAndPlayerData(id) {
-    return axios.get(`${API_URL}tournament_player_data/${id}`, { headers: authHeader() });
+    return axios.get(`${API_URL}/tournament_player_data/${id}`, { headers: authHeader() });
+  }
+
+  setUsersTournamentPlayers(selectedPlayers, userId, tournamentId) {
+    return axios.post(`${API_URL}/set_team`, {
+      selectedPlayers, userId, tournamentId,
+    }, { headers: authHeader() });
   }
 }
 
--- src/views/TournamentPlayerSeclection.vue
+++ src/views/TournamentPlayerSeclection.vue
@@ -5,6 +5,47 @@
         {{ tournament.name }} Player Selection
       </h3>
     </header>
+    <form @submit.prevent="handlePlayerSubmit">
+      <div class="table-responsive container" v-for="(tier, index) in players" :key="index">
+        <table class="table table-sm table-bordered table-hover">
+          <thead class="thead-dark">
+            <tr class="row">
+              <th class="col-1">
+              </th>
+              <th class="col-11">
+                Tier {{ tier[0].tier }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="player in tier" :key="player.id" class="row">
+              <td class="col-1">
+                <input
+                  type="radio"
+                  :id="player.id"
+                  :value="player.id"
+                  :name="player.tier"
+                  v-model="submitObject[`tier-${player.tier}`]">
+              </td>
+              <td class="col-11">
+                <label :for="player.id">
+                  {{ player.first_name }} {{ player.last_name }}
+                </label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary btn-block" :disabled="loading">
+          <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+          <span>Submit</span>
+        </button>
+      </div>
+      <div class="form-group">
+        <div v-if="message" class="alert alert-danger" role="alert">{{message}}</div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -18,24 +59,53 @@ export default {
     return {
       tournament: {
         name: '',
-        id: null
+        id: null,
       },
-      players: []
-    }
+      players: [],
+      message: '',
+      loading: false,
+      submitObject: {},
+      userId: '',
+    };
+  },
+  methods: {
+    handlePlayerSubmit() {
+      this.loading = true;
+      // Validate all fields
+
+      TournamentService
+        .setUsersTournamentPlayers(this.submitObject, this.userId, this.tournament.id).then(
+          () => {
+            // Model toaster saying success
+            // Route to team page?
+            this.$router.push('/profile');
+          }, (err) => {
+            this.message = (err.response && err.response.data)
+            || err.message
+            || err.toString();
+          },
+        );
+      this.loading = false;
+    },
   },
   mounted() {
-    this.tournament.id = this.$route.params.id
+    this.userId = this.$store.state.auth.user.id;
+    this.tournament.id = this.$route.params.id;
     TournamentService.getTournamentAndPlayerData(this.tournament.id).then(
       (response) => {
         this.tournament.name = response.data.tournamentName;
-        this.players = response.data.playerData
+        this.players = response.data.playerData;
+        this.submitObject = {};
+        for (let i = 0; i < this.players.length; i += 1) {
+          this.submitObject[`tier-${i + 1}`] = null;
+        }
       },
       (error) => {
-        this.tournament.name = (error.response && error.response.data)
+        this.message = (error.response && error.response.data)
           || error.message
           || error.toString();
-      }
-    )
-  }
+      },
+    );
+  },
 };
 </script>
