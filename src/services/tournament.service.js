de5487df21233bc7717558a8363cfe65da98944f
Jake White <jwhite52@gmail.com>
2020-05-27 17:14:50 -0400

    Leaderboard data is loaded into UI

--- src/services/tournament.service.js
+++ src/services/tournament.service.js
@@ -6,16 +6,23 @@ const API_URL = 'http://localhost:8081/api/v1';
 
 /* eslint class-methods-use-this: ["error", { "exceptMethods":
 ['getActiveTournaments', 'getTournamentAndPlayerData',
-'setTeam', 'getActiveTournamentsForAUser', 'getPastTournamentsForAUser'] }] */
+'setTeam', 'getActiveTournamentsForAUser', 'getPastTournamentsForAUser',
+'getLeaderboardData'] }] */
 class TournamentService {
   getActiveTournaments() {
     return axios.get(`${API_URL}/active_tournaments`);
   }
 
   // Get information regarding a Tournament and the Players in
-  // it. A Users team if they have one will be returned here as well
+  // it. A Users team if they have one will be returned here as well.
+  // Used to Edit and Create a Team
   getTournamentAndPlayerData(id) {
     return axios.get(`${API_URL}/tournament_player_data/${id}`, { headers: authHeader() });
+  }
+
+  // Get the User, Team, and Player data needed to display a leaderboard for a particular tournament
+  getLeaderboardData(id) {
+    return axios.get(`${API_URL}/tournament_leaderboard_data/${id}`, { headers: authHeader() });
   }
 
   // A Users team for a particular Tournament is created or updated
--- src/views/TournamentLeaderBoard.vue
+++ src/views/TournamentLeaderBoard.vue
@@ -1,29 +1,73 @@
 <template>
   <div class="container">
     <header class="jumbotron">
-      <h3>{{content}}</h3>
+      <h3>{{ tournament.name }} Leaderboard</h3>
+      <div v-if="loggedIn">
+        <h3 >{{ user.team_name }} Leaderboard</h3>
+      </div>
     </header>
+    <div v-if="loggedIn">
+      <p>
+        {{ this.user.team_name }}'s Team
+      </p>
+      <p>
+        {{ this.currentUserTeam }}
+      </p>
+    </div>
+    <div>
+      <p>
+        Leaderboard
+      </p>
+      <p>
+        {{ this.leaderboard }}
+      </p>
+    </div>
+    <div
+      v-if="message != ''"
+      class="alert alert-danger">
+      {{ message }}
+    </div>
   </div>
 </template>
 
 <script>
-import UserService from '../services/user.service';
+import TournamentService from '../services/tournament.service';
+import Tournament from '../models/tournament';
+import User from '../models/user';
 
 export default {
   name: 'Home',
   data() {
     return {
       content: 'Leaderboard',
+      tournament: new Tournament('', null),
+      user: new User('', '', ''),
+      currentUserTeam: [],
+      loggedIn: false,
+      leaderboard: {},
+      message: '',
     };
   },
   mounted() {
-    // Data needed:
-    // - Tournament name
-    // - A User's Team and their scores
-    // - Other Users Team and scores
+    this.tournament.id = this.$route.params.id;
+    if (this.$store.state.auth.user) {
+      this.user = this.$store.state.auth.user;
+      this.loggedIn = true;
+    }
 
-    // User data on top - then a leaderboard sorted team score
-
+    TournamentService.getLeaderboardData(this.tournament.id).then(
+      (response) => {
+        this.tournament.name = response.data.tournamentName.name;
+        this.leaderboard = response.data.leaderboard;
+        if (this.loggedIn) {
+          this.currentUserTeam = this.leaderboard[this.user.team_name];
+        }
+      }, (error) => {
+        this.message = (error.response && error.response.data)
+          || error.message
+          || error.toString();
+      },
+    );
   },
 };
 </script>
