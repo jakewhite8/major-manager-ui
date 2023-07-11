cb037fc6b1c5a6ddc6dc42ed038fbfe14879376f
Jake White <jwhite52@gmail.com>
2023-07-10 22:22:35 -0400

    Change Home Page wording

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,6 @@
     <header class="jumbotron">
       <h2 style="text-align: center;">Gambling Problem?</h2>
       <h4 style="text-align: center;">Call 1-800-GAMBLER</h4>
-      <h6 style="text-align: center;">Someone out there cares</h6>
     </header>
   </div>
 </template>
