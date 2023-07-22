ae41bf61a8954f1baad00d2420d5e6894f0e51bb
Jake White <jwhite52@gmail.com>
2023-07-21 21:12:55 -0400

    Move Current and Past Tournaments to Profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,7 +4,7 @@
       :customField=teamNameHeader
       :title="`Profile`" />
     <div class="profile-buttons row">
-      <div class="col-12 col-lg-4 mb-3">
+      <div class="col-12 col-lg-6 mb-3">
         <button
         type="button"
         class="btn btn-lg btn-block"
@@ -12,7 +12,7 @@
           Join Tournament
         </button>
       </div>
-      <div class="col-12 col-lg-4 mb-3">
+      <div class="col-12 col-lg-6 mb-3">
         <button
         type="button"
         class="btn btn-lg btn-block"
@@ -20,30 +20,49 @@
           Active Teams
         </button>
       </div>
-      <div class="col-12 col-lg-4">
-        <button
-        type="button"
-        class="btn btn-lg btn-block"
-        v-on:click="routeTo('past-teams')">
-          Current and Past Results
-        </button>
+    </div>
+    <div v-if="tournaments.length > 0" class="row container-margin">
+      <div class="col-12">
+        <PageTitle
+          :customField=teamNameHeader
+          :title="`Current and Past Tournaments`" />
+      </div>
+      <div class="col-12">
+        <PastTournamentsTable
+            :tournaments=tournaments
+            :loading=loading
+            :headers=headers
+            :columns=columns
+            :message=message
+            :user=user />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TournamentService from '../services/tournament.service';
 import PageTitle from '../components/PageTitle.vue';
+import PastTournamentsTable from '../components/PastTournamentsTable.vue';
+import User from '../models/user';
 
 export default {
   name: 'Profile',
+  data() {
+    return {
+      user: new User('', '', ''),
+      tournaments: [],
+      loading: false,
+      headers: [],
+      columns: [],
+      message: '',
+    };
+  },
   components: {
     PageTitle,
+    PastTournamentsTable,
   },
   computed: {
-    currentUser() {
-      return this.$store.state.auth.user;
-    },
     teamNameHeader() {
       return this.$store.state.auth.user.team_name + (this.$store.state.auth.user.team_name.charAt(this.$store.state.auth.user.team_name.length - 1) === 's' ? '\'' : '\'s');
     },
@@ -54,9 +73,27 @@ export default {
     },
   },
   mounted() {
-    if (!this.currentUser) {
+    this.user = this.$store.state.auth.user
+    if (!this.user) {
       this.$router.push('/login');
     }
+    this.loading = true;
+    TournamentService.getPastTournamentsForAUser(this.user.id).then(
+      (response) => {
+        this.tournaments = response.data;
+        // Instead of displaying start date, display the result
+        this.headers = ['Tournaments', 'Start Date'];
+        this.columns = ['name', 'start_date'];
+        this.message = '';
+        this.loading = false;
+      },
+      (error) => {
+        this.message = (error.response && error.response.data)
+          || error.message
+          || error.toString();
+        this.loading = false;
+      },
+    );
   },
 };
 </script>
@@ -70,5 +107,8 @@ export default {
   }
   .profile-buttons button:hover {
     color: white;
+  }
+  .container-margin {
+    margin-bottom: 47px;
   }
 </style>
